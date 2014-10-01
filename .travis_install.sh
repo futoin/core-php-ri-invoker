aee8ea821759d8d69ccb3a3018b6514980424495
Andrey Galkin <andrey@futoin.eu>
2014-10-01 16:23:49 +0300

    Fixed hhvm test being blocked on prompt

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if php --version | grep HHVM | read; then
-    sudo add-apt-repository ppa:ondrej/php5 &&\
+    sudo add-apt-repository -y ppa:ondrej/php5 &&\
         sudo apt-get update && \
         sudo apt-get install php5-cli -y
 else
